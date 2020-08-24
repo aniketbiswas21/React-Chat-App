be909af1526e681deba90a8a81f3bc090d39c5ff
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-08-24 17:12:14 +0530

    production fix

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
     const { user, error } = addUser({ id: socket.id, name, room });
